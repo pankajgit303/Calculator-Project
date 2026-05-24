94ae07e14b1f49ce5fca4bc0a8d8e8bc6fc893bb
Pankaj Singha <pankajsingha303@gmail.com>
2026-05-24 13:45:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let input = document.getElementById('inputBox');
+
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+
+    button.addEventListener('click', (e) => {
+
+        let value = e.target.innerHTML;
+
+
+        if (value == '=') {
+
+            try {
+                string = eval(string).toString();
+                input.value = string;
+            }
+
+            catch {
+                input.value = "Error";
+                string = "";
+            }
+        }
+
+        
+        else if (value == 'AC') {
+            string = "";
+            input.value = string;
+        }
+
+    
+        else if (value == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+
+        
+        else if (value == '%') {
+            string += '%';
+            input.value = string;
+        }
+
+        // Other buttons
+        else {
+            string += value;
+            input.value = string;
+        }
+
+    });
+
+});
